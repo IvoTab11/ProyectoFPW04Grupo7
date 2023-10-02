a06376879acef75b2ff08295d1c22cdcce9f8b09
IvoTab11 <142843560+IvoTab11@users.noreply.github.com>
2023-10-02 16:05:36 -0300

    Pantalla de inicio modificada. Ivo Tabarcache

--- src/escenas/Escena1.js
+++ src/escenas/Escena1.js
@@ -11,12 +11,16 @@ class Escena1 extends Phaser.Scene{
 
     create(){
         this.add.image(400, 300, 'inicio');
+        this.input.keyboard.on('keydown-SPACE', function () {
+            // Cambiar a la Escena2
+            this.scene.start('Escena2');
+        }, this);
     }
 
-    update(){
-        if(Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE))){
-            this.scene.start('Escena2');
-        }
-    }
+    // update(){
+    //     if(Phaser.Input.Keyboard.JustDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE))){
+    //         this.scene.start('Escena2');
+    //     }
+    // }
 }
 export default Escena1;
